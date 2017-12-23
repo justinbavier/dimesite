1c6c5a99c46a1d21472fbb8ab89e4f0ad120bced
justinbaver <jbavier123@gmail.com>
2017-12-23 11:15:11 -0700

    Added static.json to hopefully fix routing

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get('/api', function (req, res) {
   console.log('you');
 });
 
-var mailchimpInstance   = process.env.MAILCHIMP_INSTANCE, 
+var mailchimpInstance   = process.env.MAILCHIMP_INSTANCE,
     listUniqueId        = process.env.NEWSLETTER_ID,
     mailchimpApiKey     = process.env.MAILCHIMP_API_KEY;
 
@@ -49,7 +49,7 @@ app.post('/api/signup', function (req, res) {
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('/*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../react-ui/public', 'index.html'));
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
 app.listen(PORT, function () {
